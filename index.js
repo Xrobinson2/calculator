dc46c8b17c3282ee86b05fc6004645e68f80ca4d
Xavier Robinson <x.robinson5041@outlook.com>
2024-04-26 16:21:41 -0400

    comeback and finish scripting

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+
+
+
+window.onload = init;
+
+function init() {
+  const greetBtn = document.getElementById("greetBtn");
+  greetBtn.onclick = onGreetUserBtnClicked;   
+}
+
+// Number fields
+
+function onGreetUserBtnClicked() {
+    const nameField = document.getElementById("nameField").value
+
+    alert(message);
+}
+
+function onGreetUserBtnClicked() {
+    const nameField = document.getElementById("nameField").value
+  
+    alert(message);
+}
+
+
+// Here on down are the order of operations buttons
+
+function init() {
+    const addBtn = document.getElementById("addBtn").value;
+    const multiBtn = document.getElementById("multiBtn").value;
+    const subBtn = document.getElementById("subBtn").value;
+    const divBtn = document.getElementById("divBtn").value;
+
+    
+
+  
+
+
+
+
+// Answer Fields
+
+function onGreetUserBtnClicked() {
+    const nameField = document.getElementById("nameField").value
+  let message = "Hello! " + nameField
+    alert(message);
+}
+
